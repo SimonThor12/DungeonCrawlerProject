15c6d999834b27a2152339fbb6c99a6e535f5852
SimonThor12 <92580924+SimonThor12@users.noreply.github.com>
2023-10-17 18:40:50 +0200

    simons gameengine

--- PlayerCharacter.cs
+++ PlayerCharacter.cs
@@ -5,6 +5,7 @@
     public string Name { get; set; }
     public int Health { get; set; }
     public ICharacterAttackBevaviour AttackBevaviour { get; set; }
+
     public PlayerCharacter(string name, int health, ICharacterAttackBevaviour attackBevaviour)
     {
       Name = name;
