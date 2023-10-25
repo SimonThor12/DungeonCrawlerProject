50cab97c8d268b9c2faafb2690a23c8cebc6b352
Modulen <alexander.oskarsson@gmail.com>
2023-10-25 11:25:35 +0200

    fixade inventory

--- GameEngine.cs
+++ GameEngine.cs
@@ -92,15 +92,20 @@
     private void HandleUserDoorAction()
     {
       Console.Clear();
-      Console.WriteLine("What do you want to do?");
+      TypeTextWithDelay("You are standing infront of the door.");
+      TypeTextWithDelay("What do you want to do?");
       string action = Console.ReadLine();
       if (action.ToLower().Trim() != "open" && action.ToLower().Trim() != "open door" && action.ToLower().Trim() != "hit" && action.ToLower().Trim() != "use inventory" && action.ToLower().Trim() != "use potion" && action.ToLower().Trim() != "open inventory")
       {
         Console.WriteLine("Try something else");
         HandleUserDoorAction();
       }
-      else if (action.ToLower().Trim() == "use inventory" || action.ToLower().Trim() == "use potion" || action.ToLower().Trim() == "open inventory")
-      {
+       else if (action.ToLower().Trim() == "use inventory" || action.ToLower().Trim() == "use potion" || action.ToLower().Trim() == "open inventory")
+            {
+                UseInventory();
+                HandleUserDoorAction();
+            }
+            /*{
         Console.WriteLine("You open your inventory");
         if (currentPlayer.personalInventory.Items.Count == 0)
         {
@@ -146,7 +151,7 @@
         }
         Console.WriteLine("You leave the inventory and go back to the door.");
         HandleUserDoorAction();
-      }
+      }*/
       else if (action.ToLower().Trim() == "hit")
       {
         Console.WriteLine($"You hit with {currentPlayer.equipedWeapon.Name} in the door opening");
@@ -210,5 +215,53 @@
         return name;
       }
     }
-  }
+        private void UseInventory()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("You open your inventory");
+            if (currentPlayer.personalInventory.Items.Count == 0)
+            {
+                Console.WriteLine("Your inventory is empty.");
+                return;
+            }
+
+            int invNum = 1;
+            foreach (var item in currentPlayer.personalInventory.Items)
+            {
+                Console.WriteLine($"{invNum}: {item.Name}");
+                invNum++;
+            }
+
+            Console.WriteLine("What do you want to use? Or press 'B' to go back.");
+
+            string choice = Console.ReadLine().ToLower().Trim();
+
+            if (choice == "b")
+            {
+                Console.WriteLine("You leave your inventory and go back to the door");
+                return;
+            }
+
+            if (int.TryParse(choice, out int itemNumber) && itemNumber >= 1 && itemNumber <= currentPlayer.personalInventory.Items.Count)
+            {
+                var selectedItem = currentPlayer.personalInventory.GetItem(itemNumber - 1);
+                if (selectedItem != null)
+                {
+                    selectedItem.UseItem(currentPlayer);
+                    currentPlayer.personalInventory.RemoveItem(selectedItem);
+                    UseInventory();
+                }
+                else
+                {
+                    Console.WriteLine("The selected item cannot be used.");
+                    UseInventory();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+                UseInventory(); // Call the UseInventory method again for another choice.
+            }
+        }
+    }
 }
