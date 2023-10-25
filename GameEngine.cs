5c7bc5de2b509cf8187c662bbb60d5ea0f253942
Modulen <alexander.oskarsson@gmail.com>
2023-10-25 11:48:37 +0200

    test

--- GameEngine.cs
+++ GameEngine.cs
@@ -102,56 +102,10 @@
       }
        else if (action.ToLower().Trim() == "use inventory" || action.ToLower().Trim() == "use potion" || action.ToLower().Trim() == "open inventory")
             {
+                Console.WriteLine("You open your inventory");
                 UseInventory();
                 HandleUserDoorAction();
             }
-            /*{
-        Console.WriteLine("You open your inventory");
-        if (currentPlayer.personalInventory.Items.Count == 0)
-        {
-          Console.WriteLine("Your inventory is empty.");
-          HandleUserDoorAction();
-          return;
-        }
-
-        int invNum = 1;
-        foreach (var item in currentPlayer.personalInventory.Items)
-        {
-          Console.WriteLine($"{invNum}: {item.Name}");
-          invNum++;
-        }
-
-        Console.WriteLine("What do you want to use? Or press 'B' to go back.");
-
-        string choice = Console.ReadLine().ToLower().Trim();
-
-        if (choice == "b")
-        {
-          Console.WriteLine("You leave your inventory and go back to the door");
-          HandleUserDoorAction();
-          return;
-        }
-
-        if (int.TryParse(choice, out int itemNumber) && itemNumber >= 1 && itemNumber <= currentPlayer.personalInventory.Items.Count)
-        {
-          var selectedItem = currentPlayer.personalInventory.GetItem(itemNumber - 1);
-          if (selectedItem != null)
-          {
-            selectedItem.UseItem(currentPlayer);
-            currentPlayer.personalInventory.RemoveItem(selectedItem);
-          }
-          else
-          {
-            Console.WriteLine("The selected item cannot be used.");
-          }
-        }
-        else
-        {
-          Console.WriteLine("Invalid choice.");
-        }
-        Console.WriteLine("You leave the inventory and go back to the door.");
-        HandleUserDoorAction();
-      }*/
       else if (action.ToLower().Trim() == "hit")
       {
         Console.WriteLine($"You hit with {currentPlayer.equipedWeapon.Name} in the door opening");
@@ -218,10 +172,11 @@
         private void UseInventory()
         {
             Console.WriteLine("");
-            Console.WriteLine("You open your inventory");
+            Console.WriteLine("Inventory:");
             if (currentPlayer.personalInventory.Items.Count == 0)
             {
                 Console.WriteLine("Your inventory is empty.");
+                Thread.Sleep(1000);
                 return;
             }
 
@@ -239,6 +194,7 @@
             if (choice == "b")
             {
                 Console.WriteLine("You leave your inventory and go back to the door");
+                Thread.Sleep(1000);
                 return;
             }
 
